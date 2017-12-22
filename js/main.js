ce041fb2cca017f0e164193858daf3d76852ff8a
chandra reddy <chandra reddy>
2017-12-22 18:12:12 +0530

    responsive design

--- js/main.js
+++ js/main.js
@@ -96,6 +96,49 @@ jQuery(document).ready(function() {
         label: '',
     });
 
+    // steller js for parralax animation
+
+    $('#main').stellar();
+
+    // waypoint js
+
+    $(".animation").each(function() {
+        var waypoint = new Waypoint({
+            element: this,
+            handler: function(direction) {
+                let animation = $(this.element).attr("data-animation");
+                //console.log(animation);
+                $(this.element).css("opacity", "1");
+                $(this.element).addClass("animated " + animation);
+            },
+            offset: "75%"
+        });
+
+    });
+
+    // smmoth scroll
+    $('a').smoothScroll();
+    //contact-form
+    $('.contact #submit').on('click', function(e) {
+        e.preventDefault();
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var subject = $('#subject').val();
+        var message = $('#message').val();
+        var form = new Array({ 'name': name, 'email': email, 'subject': subject, 'message': message });
+
+        $.ajax({
+            type: 'POST',
+            url: "contact.php",
+            data: ({ 'action': 'contact', 'form': form })
+        }).done(function(data) {
+            $('#contact .result').html(data);
+
+            $(".contact-form")[0].reset();
+
+        });
 
 
+
+    });
 });
